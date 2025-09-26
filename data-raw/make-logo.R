79347bd7587169b2a92463a53545e546dffd8e08
Camilo Rodriguez <camo.rodriguez@gmail.com>
2025-09-26 16:52:08 +0200

    Logo creation

---
+++ data-raw/make-logo.R
@@ -0,0 +1,15 @@
+library(hexSticker)
+img <- "data-raw/frog_logo.png"
+
+sticker(
+  subplot = img,
+  package = "poisonfrogs",
+  p_size = 20,
+  filename = "man/figures/logo.png",
+  s_x = 1,
+  s_y = .75,
+  s_width = .51,
+  h_fill = "#A9B4C6"
+)
+
+?sticker
